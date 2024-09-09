378489ed4d24a35c1fb9c90e20669a54233f0dba
Tim-PF <tim.pfops@gmail.com>
2024-09-09 12:51:21 +0200

    Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show, Hide } from "@chakra-ui/react";
+
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, // 1024 px
+      }}
+    >
+      <GridItem gridArea={"nav"} bg={"blue"}>
+        {" "}
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem gridArea={"aside"} bg={"gold"}>
+          {" "}
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem gridArea={"main"} bg={"green"}>
+        {" "}
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
